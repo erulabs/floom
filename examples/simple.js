9c4385a32c60f4a8e45a01bfa6e5681c9134cdec
erulabs <seandon.mooy@gmail.com>
2015-04-19 04:44:02 -0500

    output filtering, better debugging, simplify .simple

--- examples/simple.js
+++ examples/simple.js
@@ -2,22 +2,6 @@
 
 var ops = require('./../index.js');
 
-// var nodes = [
-//   {
-//     name: 'erulabs.com',
-//     ssh: {
-//       username: 'seandon'
-//     }
-//   }
-// ];
-//
-// ops.log.level = 'debug';
-//
-// ops.nodes(nodes)
-//   .pipe(ops.save())
-//   .pipe(ops.exec('ls -al'))
-//   .pipe(ops.end());
-
 var nodes = [
   {
     name: 'erulabs.com',
@@ -26,8 +10,27 @@ var nodes = [
     }
   }
 ];
+
+ops.log.level = 'debug';
+ops.persistent = true;
+
 ops.nodes(nodes)
-  .pipe(ops.simple(function (node, callback) {
-    callback();
+  .pipe(ops.save())
+  .pipe(ops.exec('ls -al', function (output) {
+    console.log(output);
   }))
   .pipe(ops.disconnect());
+
+//var nodes = [
+//  {
+//    name: 'erulabs.com',
+//    ssh: {
+//      username: 'seandon'
+//    }
+//  }
+//];
+//ops.nodes(nodes)
+//  .pipe(ops.simple(function (node, callback) {
+//    callback();
+//  }))
+//  .pipe(ops.disconnect());
